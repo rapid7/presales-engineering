46cee77d5eba0091d8688f438206ab5cc0c7db5e
thonker-r7 <tim@private.local>
2020-09-21 10:38:49 -0400

    Initial commit for scan engine bootstrap, generic to Linux

---
+++ InsightVM/bootstrap-linux-insightvm-scan-engine.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Tim H 2020
+# This script installs and configures an InsightVM scan engine and pairs it to an existing InsightVM console.
+
+# exit if anything fails, do not continue
+set -e
+
+# define variables for deployment
+console="example.company.com"
+secret="A1A1-B2B2-C3C3-D4D4-E5E5-F6F6-G7G7-H8H8"
+
+
+if [ ! "$USER" == "root" ]; then
+    echo "This script must be run as root, aborting."
+    exit 1
+fi
+
+# Download the installer for both InsightVM and Nexpose (same) and hashsum file
+# This makes sure you're getting the latest installer
+wget --quiet https://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin \
+		http://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin.sha512sum
+
+# Check the integrity of the download
+sha512sum --check Rapid7Setup-Linux64.bin.sha512sum
+
+# Mark installer as executable
+chmod u+x Rapid7Setup-Linux64.bin
+
+# Install the InsightVM scan engine and specify the InsightVM console and shared secret to pair with:
+./InsightVM/Rapid7Setup-Linux64.bin -q -overwrite -Vfirstname='FirstName' -Vlastname='LastName' \
+    -Vcompany='Rapid7' -Vusername='nxadmin' -Vpassword1='nxadmin' -Vpassword2='nxadmin' \
+    -Vsys.component.typical\$Boolean=false -Vsys.component.engine\$Boolean=true \
+    -VinitService\$Boolean=true \
+    -VcommunicationDirectionChoice\$Integer=0 \
+    -VconsoleAddress="$console" -VconsoleDetailPort='40815' -VsharedSecret="$secret"

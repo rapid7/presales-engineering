fc953a202458589be3ada4b4510dbf084834c535
thonker-r7 <tim@private.local>
2020-09-21 10:43:16 -0400

    added comments

--- DivvyCloud/upgrade-divvy.sh
+++ DivvyCloud/upgrade-divvy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+#
+# Description:
+#   Script to upgrade DivvyCloud consoles since they can't do it on their own yet
+#
 # crontab for Sunday nights at 11pm, run as root
 #0 23 * * 7 /root/upgrade-divvy.sh
 
 # bomb out if any errors occur
 set -e
+
+echo "Starting to upgrade Divvy containers. Divvy will be restarted."
 
 # bail if not root or sudo
 if [[ $EUID -ne 0 ]]; then
@@ -11,7 +17,9 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-cd /divvycloud/
+cd /divvycloud/ || exit 1
 docker-compose down
 docker-compose pull
 docker-compose up -d
+
+echo "finished updating Divvy containers."

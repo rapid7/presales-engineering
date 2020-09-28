4fac48c3d0a93dfe410495884876daae244b0e37
thonker-r7 <tim@private.local>
2020-09-28 11:04:30 -0400

    additional comments

--- InsightVM/test-approve_scan_engine_ip.sh
+++ InsightVM/test-approve_scan_engine_ip.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-#
-#	test script for approve_scan_engines_ip.sh
-#	Loops through a provided consoles.xml file and enables all the IPs
+#	Tim H 2020
+#	unit test script for approve_scan_engines_ip.sh
+#	Loops through a provided consoles.xml file and enables all the IPs.
+#	
+# 	Example usage: does not take any parameters
+#		./test-approve_scan_engine_ip.sh
+
 set -e
 
 CONSOLES_FILE="example_consoles.xml"

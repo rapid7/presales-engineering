ee577abb06a53afa26d4476e7de5ce3c978cdbdb
thonker-r7 <tim@private.local>
2020-12-30 09:14:05 -0500

    added more searches and regexes

--- InsightAppSec/insightappsec-log-troubleshooting-tips.sh
+++ InsightAppSec/insightappsec-log-troubleshooting-tips.sh
@@ -5,16 +5,44 @@
 echo "This is a series of notes, not designed to be executed directly"
 exit 1
 
-LOG_FILES_FULL_PATH=$1
+LOG_FILES_FULL_PATH="$HOME/Downloads/logfiles"
+
 
 
 grep -i " error " "$LOG_FILES_FULL_PATH/operation_00000.log" 
-grep -i " error: " "$LOG_FILES_FULL_PATH/operation_00000.log" | grep -v "Could not execute event in the"
+grep -n -i " error: " "$LOG_FILES_FULL_PATH/operation_00000.log" | grep -v "Could not execute event in the"
+
+egrep "(?:[4-5]|\d{2,})\b Response" "$LOG_FILES_FULL_PATH/operation_00000.log" 
+egrep "(?:[4-5]|\d{2,})\b Response" "$LOG_FILES_FULL_PATH/operation_00000.log" | cut -d " " -f2- | sort --unique
+grep "401 Response" "operation_00000.log" | cut -d ":" -f5- | sort --unique
+
+
+
+
+grep "HTTP/1.1 401" --before-context=10 --after-context=20 -m 1 traffic_*.log
+grep "HTTP/1.1 401" -H --line-number -C 10 --max-count=1 traffic_*.log
+
+
+grep "GET /api/" traffic_*.log | sort --unique
+
+grep "/api/goals" -H --line-number -C 10 traffic_*.log
+
+
+grep "HTTP/1.1 401" -H --line-number -C 10 traffic_*.log
+
+#Grep for "Response Code: 401" in the trafficemetadata file
+#/api/goals
+
+
 
 grep -i "\[warn\]" 	"$LOG_FILES_FULL_PATH/operation_00000.log" 
 grep -i "\[error\]" 	"$LOG_FILES_FULL_PATH/operation_00000.log" 
 
 grep -i "seconds" 	"$LOG_FILES_FULL_PATH/operation_00000.log" 
+
+grep "GET /api/goals" -H --line-number traffic_*.log
+grep "GET /api/goals" -C 40 traffic_*.log
+
 
 grep "Response Time: "		"$LOG_FILES_FULL_PATH/trafficmetadata_00000.log" | cut --delimiter="\ " -f2
 

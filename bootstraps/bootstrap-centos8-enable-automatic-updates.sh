e7f61a4703a1e2465603993cb00966851b512360
thonker-r7 <tim@private.local>
2021-02-28 00:30:43 -0500

    migrating customer-irrelevant files to another repo

--- bootstraps/bootstrap-centos8-enable-automatic-updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Tim H 2020
-# enable automatic updates in CentOS 8
-
-dnf install dnf-automatic
-rpm -qi dnf-automatic
-vim /etc/dnf/automatic.conf
-systemctl enable --now dnf-automatic.timer
-systemctl list-timers *dnf-*

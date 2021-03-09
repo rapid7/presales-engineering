6bf55d792167fed9563b7e97d981a649153c6f06
thonker-r7 <tim@private.local>
2021-03-09 13:31:01 -0500

    made automated so it can be run headless in non-interactive mode

--- bootstraps/bootstrap-centos7-enable-automaticupdates.sh
+++ bootstraps/bootstrap-centos7-enable-automaticupdates.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 # Tim H 2020
 # enable automatic updates in CentOS 7
-#   still requires manual work in the yum-cron.conf file
 
-echo "this script is not yet ready to be automatically run."
-exit 1
+# bail if not root or sudo
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 
+# install dependencies
 yum install -y yum-cron vim
+
+# check if it is running and setup already:
+grep "apply_updates\|update_cmd" /etc/yum/yum-cron.conf
+systemctl status yum-cron.service
+
+# change the settings
 #apply_updates = no    should be yes
 #update_cmd = default  should be security
-vim /etc/yum/yum-cron.conf
-#TODO: fix the enabled via sed instead of manually via VIM
+sed -i 's/apply_updates = no/apply_updates = yes/g' /etc/yum/yum-cron.conf
+sed -i 's/update_cmd = default/update_cmd = security/g' /etc/yum/yum-cron.conf
+
+# verify changes
+grep "apply_updates\|update_cmd" /etc/yum/yum-cron.conf
+
+# start the service, set it to autostart, verify it is running
 systemctl start  yum-cron.service
 systemctl enable yum-cron.service
 systemctl status yum-cron.service

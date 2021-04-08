04d43008169f3ec7d845af48f48ccba8f30fe52b
thonker-r7 <tim@private.local>
2021-04-08 13:08:54 -0400

    switched wget to curl, corrected installer command so it actually works with pairing scan engines

--- InsightVM/bootstrap-ubuntu-insightvm-console.sh
+++ InsightVM/bootstrap-ubuntu-insightvm-console.sh
@@ -10,31 +10,27 @@ cd "$HOME" || cd /root
 
 #TODO: use Curl instead of wget, avoid the logging CPU slowdown that wget seems to be causing
 apt-get update -q
-apt-get -y install wget
 
 # Download the installer for both InsightVM and Nexpose (same) and MD5 hashsum file
 # This makes sure you're getting the latest installer
-wget https://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin https://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin.sha512sum
+curl -o Rapid7Setup-Linux64.bin             https://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin
+curl -o Rapid7Setup-Linux64.bin.sha512sum   http://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin.sha512sum
 
 # Check the integrity of the download
 sha512sum --check Rapid7Setup-Linux64.bin.sha512sum
 
 # Mark installer as executable
 chmod u+x Rapid7Setup-Linux64.bin
-
-# install InsightVM, but don't start the service yet
+# install InsightVM console, but don't start the service yet
 # unfortunately these command line arguments aren't publicly documented
-./Rapid7Setup-Linux64.bin -q -overwrite -Djava.net.useSystemProxies=false \
+./Rapid7Setup-Linux64.bin -q -overwrite  \
+    -Djava.net.useSystemProxies=false \
     -Vfirstname='NAME' \
     -Vlastname='NAME' \
     -Vcompany='COMPANY' \
     -Vusername='nxadmin' \
     -Vpassword1='nxpassword' \
     -Vpassword2='nxpassword' \
-    "-Vsys.component.typical$Boolean=true" \
-    "-Vsys.component.engine$Boolean=false" \
-    "-VcommunicationDirectionChoice$Integer=1" \
-    "-VinitService$Boolean=false"
 
 # follow documentation, here is where you'd enable FIPS mode if needed.
 # https://docs.rapid7.com/insightvm/enabling-fips-mode/#enabling-fips-mode

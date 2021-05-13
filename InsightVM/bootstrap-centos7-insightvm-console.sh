b4f5cb9bb20c88cfa3543ee0a08209cad4bb6c86
thonker-r7 <tim@private.local>
2021-05-13 13:14:23 -0400

    additional comments for copy and paste people

--- InsightVM/bootstrap-centos7-insightvm-console.sh
+++ InsightVM/bootstrap-centos7-insightvm-console.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Tim H 2021
-#   Bootstrap installer for InsightVM Console on CentOS 7
+#   Bootstrap installer for InsightVM Console on CentOS 7 64-bit
 #   sets default UI creds as nxadmin/nxpassword
 #
 # Common mistakes:
 # 1) Forgetting to DISABLE SE LINUX
 # 2) Forgetting to add additional engines to the license count - default licenses are often just 1 scan engine
 # 3) Forgetting to open firewall to allow incoming connections on TCP 3780 and 40815
+# 4) Not meeting the bare minimum system requirements (2 cores, 8 GB RAM, 80 GB HDD)
 
 # exit if anything fails, do not continue
 set -e
@@ -19,28 +20,30 @@ fi
 # move into root user's home directory
 cd "$HOME" || cd /root
 
-# TODO: make sure that selinux is in fact disabled before running installing
+# disable SELinux immediately and on all future reboots, required for InsightVM to work properly
 sudo setenforce 0
 sudo echo "SELINUX=disabled
 SELINUXTYPE=targeted" > /etc/selinux/config
+# TODO: make sure that selinux is in fact disabled before running installing
 
-# update local package list
+# OPTIONAL - update local package list from Yum, but don't download any updates
 sudo yum makecache
 
-# install common tools for troubleshooting later. Not mandatory.
+# OPTIONAL - install common tools for troubleshooting later. Not mandatory but recommended for troubleshooting/config
 sudo yum install -y atop bind-utils coreutils curl \
     glances grep htop iftop iotop lsof \
     mlocate nc net-tools nload nmap ntpdate open-vm-tools \
     openldap-devel openssh-server openssl openssl-devel \
     screen sudo sysstat tar tcpdump tcpflow \
     telnet traceroute tree unzip vim vim-enhanced wget which
-    
-sudo yum update -q -y
 
-# Download the installer for both InsightVM and Nexpose (same) and MD5 hashsum file
-# This makes sure you're getting the latest installer
+# OPTIONAL - install system updates before installing InsightVM
+#sudo yum update -q -y
+
+# Download the installer for both InsightVM and Nexpose (same) hashsum file
+# This makes sure you're always getting the latest installer
 curl -o Rapid7Setup-Linux64.bin             https://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin
-curl -o Rapid7Setup-Linux64.bin.sha512sum   http://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin.sha512sum
+curl -o Rapid7Setup-Linux64.bin.sha512sum   https://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin.sha512sum
 
 # check the integrity of the IVM installer delete it if it doesn't match
 sha512sum --check Rapid7Setup-Linux64.bin.sha512sum || rm -f Rapid7Setup-Linux64.bin
@@ -50,17 +53,23 @@ chmod u+x Rapid7Setup-Linux64.bin
 
 # install InsightVM console, but don't start the service yet
 # unfortunately these command line arguments aren't publicly documented
+# the Vfirstname, Vlastname and Vcompany are only used for the self-signed SSL certificate
+# this is the non-interactive version that will automatically install InsightVM console
+# if you want to do the interactive install, just do:
+#   sudo ./Rapid7Setup-Linux64.bin
 sudo ./Rapid7Setup-Linux64.bin -q -overwrite  \
     -Djava.net.useSystemProxies=false \
-    -Vfirstname='NAME' \
-    -Vlastname='NAME' \
-    -Vcompany='COMPANY' \
+    -Vfirstname='Rapid7 InsightVM Console' \
+    -Vlastname='POC' \
+    -Vcompany='Rapid7' \
     -Vusername='nxadmin' \
     -Vpassword1='nxpassword' \
-    -Vpassword2='nxpassword' \
+    -Vpassword2='nxpassword'
 
-# start it up, not started by default
+# start the InsightVM console service, not started by default
 sudo systemctl start nexposeconsole.service
+
+# END OF REQUIRED STEPS, the following are optional for automated deployments:
 
 # wait for a long time: 45 minutes, should be enough time for even a slow system to finish starting the service for the first time
 sleep 45m
--- InsightVM/bootstrap-ubuntu-insightvm-console.sh
+++ InsightVM/bootstrap-ubuntu-insightvm-console.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# AWS Bootstrap script for installing InsightVM/Nexpose on Ubuntu 16/18
-# Sets the default web console username/password to nxadmin/nxpassword
+# Bootstrap script for installing InsightVM/Nexpose on Ubuntu 64-bit
+#   sets default UI creds as nxadmin/nxpassword
+#
+# Common mistakes:
+# 1) Forgetting to add additional engines to the license count - default licenses are often just 1 scan engine
+# 2) Forgetting to open firewall to allow incoming connections on TCP 3780 and 40815
+# 3) Not meeting the bare minimum system requirements (2 cores, 8 GB RAM, 80 GB HDD)
 
 # exit if anything fails, do not continue
 set -e
@@ -8,29 +13,35 @@ set -e
 # move into root user's home directory
 cd "$HOME" || cd /root
 
-#TODO: use Curl instead of wget, avoid the logging CPU slowdown that wget seems to be causing
-sudo apt-get update -q
+# OPTIONAL - update the package list
+#sudo apt-get update -q
 
 # Download the installer for both InsightVM and Nexpose (same) and MD5 hashsum file
 # This makes sure you're getting the latest installer
 curl -o Rapid7Setup-Linux64.bin             https://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin
 curl -o Rapid7Setup-Linux64.bin.sha512sum   http://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin.sha512sum
 
-# Check the integrity of the download
+# Check the integrity of the download, makes sure IPSes or SSL proxies didn't mess with the installer.
 sha512sum --check Rapid7Setup-Linux64.bin.sha512sum
 
 # Mark installer as executable
 chmod u+x Rapid7Setup-Linux64.bin
+
+
 # install InsightVM console, but don't start the service yet
 # unfortunately these command line arguments aren't publicly documented
+# the Vfirstname, Vlastname and Vcompany are only used for the self-signed SSL certificate
+# this is the non-interactive version that will automatically install InsightVM console
+# if you want to do the interactive install, just do:
+#   sudo ./Rapid7Setup-Linux64.bin
 sudo ./Rapid7Setup-Linux64.bin -q -overwrite  \
     -Djava.net.useSystemProxies=false \
-    -Vfirstname='NAME' \
-    -Vlastname='NAME' \
-    -Vcompany='COMPANY' \
+    -Vfirstname='Rapid7 InsightVM Console' \
+    -Vlastname='POC' \
+    -Vcompany='Rapid7' \
     -Vusername='nxadmin' \
     -Vpassword1='nxpassword' \
-    -Vpassword2='nxpassword' \
+    -Vpassword2='nxpassword'
 
 # follow documentation, here is where you'd enable FIPS mode if needed.
 # https://docs.rapid7.com/insightvm/enabling-fips-mode/#enabling-fips-mode

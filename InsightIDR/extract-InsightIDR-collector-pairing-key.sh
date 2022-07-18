5bee1a9359c12e88a99023e061e32e6f82a80f17
thonker-r7 <23639701+thonker-r7@users.noreply.github.com>
2022-07-18 10:37:48 -0400

    added dependencies for Ubuntu

--- InsightIDR/extract-InsightIDR-collector-pairing-key.sh
+++ InsightIDR/extract-InsightIDR-collector-pairing-key.sh
@@ -3,8 +3,11 @@
 # extract the InsightIDR collector pairing key as plain text with no formatting
 # can be done after installing the InsightIDR collector and starting the service for the first time.
 
-# install dependency
-sudo yum install libxml2
+# install dependencies in CentOS / RHEL
+# sudo yum install libxml2
+
+# install dependencies in Ubuntu:
+# sudo apt-get install libxml2-utils
 
 # use XML lint utility to extract the key via XPATH reference
 # use the text() method to remove formatting

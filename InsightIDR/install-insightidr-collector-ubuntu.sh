f0e07a8ad98eb2977c28f283b2974e5d7cddbf42
thonker-r7 <23639701+thonker-r7@users.noreply.github.com>
2022-07-18 10:38:28 -0400

    added comments, documentation, and proper sudo usage

--- InsightIDR/install-insightidr-collector-ubuntu.sh
+++ InsightIDR/install-insightidr-collector-ubuntu.sh
@@ -3,10 +3,10 @@
 # Installing the InsightIDR collector on Ubuntu Linux
 
 # pull down the latest list of updates to the next command will work
-apt-get update
+sudo apt-get update
 
 # install dependencies 
-apt-get install -y wget sudo 
+sudo apt-get install -y wget sudo 
 
 # go to user's home directory
 cd "$HOME" || exit 1
@@ -19,3 +19,12 @@ chmod u+x Collector_installer.sh
 
 # run it
 sudo ./Collector_installer.sh
+
+# service should automatically start after install
+# you can verify it is running here:
+sudo service collector status
+
+# next you will have to pair the collector.
+# you can programatically extract the collector's pairing key by following
+# this script:
+# https://github.com/rapid7/presales-engineering/blob/tim-dev/InsightIDR/extract-InsightIDR-collector-pairing-key.sh

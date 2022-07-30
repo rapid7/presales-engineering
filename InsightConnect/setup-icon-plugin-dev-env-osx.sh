94822f2923b2b25d157b4717be8c4c4ddf67b7de
thonker-r7 <23639701+thonker-r7@users.noreply.github.com>
2022-07-30 04:24:01 -0400

    working version compat with SentinelOne agent

--- InsightConnect/setup-icon-plugin-dev-env-osx.sh
+++ InsightConnect/setup-icon-plugin-dev-env-osx.sh
@@ -13,20 +13,38 @@
 
 set -e
 
+# install brew if it's not already installed
 brew update
 brew upgrade
+
+# reinstall all installed versions of Python from Brew:
+# helps fix minor issues that creep up over the years:
+brew list --formula -1 | grep 'python@' | xargs -n1 brew reinstall
 
 # verify there are no major issues with brew before proceeding:
 brew doctor
 
-# installs "Command Line Tools for Xcode"
-# must download latest list of updates and install them before running the 
-# next commands
-# Otherwise you'll get this error: 
-#   "Error: Your Command Line Tools are too outdated."
-# this may need to be after the xcode-select --install command
-softwareupdate --all --install --force
+# other dependencies:
+# verify that python is at least v 3.0 or later
+python3 --version
+# check "python" version - you may get prompted for installing dev tools
+# that will take 90-120 minutes even thought it says 23 min
+python --version
 
+# upgrade PIP, both sudo and regular are needed for some dumb reason
+sudo pip3 install --upgrade pip
+pip3 install --upgrade pip
+
+# verify Docker is installed:
+docker --version
+
+# verify make is installed:
+make --version
+
+# install "Command Line Tools for Xcode"
+# this takes a few hours too, but the command line looks like it freezes
+# and it seems like there is no progress.
+softwareupdate --all --install --force
 
 # see where (if any) current Dev tools are installed:
 xcode-select --print-path
@@ -36,35 +54,47 @@ xcode-select --print-path
 # sudo rm -rf /Library/Developer/CommandLineTools
 
 # install Xcode Command Line Tools:
-#   https://www.freecodecamp.org/news/install-xcode-command-line-tools/
 # pops up interactive prompt
 # this will take a long time to download it:
 # nominally 60 minutes, maybe hours over a slow connection
 xcode-select --install
 
-# sudo xcode-select --switch /Applications/Xcode.app
+# remove old or broken version of stuff:
+rm -Rf /usr/local/Homebrew/Library/Taps/rapid7/
 
 # install the Rapid7 InsightConnect plugin for Brew
-brew tap rapid7/icon-plugin-homebrew https://github.com/rapid7/icon-plugin-homebrew
-brew install icon-plugin
+# this is broken for some reason
+# brew tap --verbose rapid7/icon-plugin-homebrew https://github.com/rapid7/icon-plugin-homebrew
+# Error: Invalid formula: /usr/local/Homebrew/Library/Taps/rapid7/homebrew-icon-plugin-homebrew/icon-plugin.rb
+# icon-plugin: wrong number of arguments (given 1, expected 0)
+# Error: Cannot tap rapid7/icon-plugin-homebrew: invalid syntax in tap!
+# brew install icon-plugin
+
+# manual OS X install of icon-plugin:
+cd "$HOME/Downloads" || exit 5
+wget "https://github.com/rapid7/icon-plugin-homebrew/releases/download/v4.11.6/icon-plugin_v4.11.6_Darwin_x86_64.tar.gz"
+tar -xf icon-plugin_v4.11.6_Darwin_x86_64.tar.gz
+cp icon-plugin /usr/local/bin/
 
 # change into a directory where you download GitHub repos
 # you may need to make this directory or change it
 cd "$HOME/source_code/third_party" || exit 1
 
 # download the InsightConnect GitHub repository
-# SLOW: this also takes a few minutes to run
+# SLOW: takes a few minutes to run
 git clone https://github.com/rapid7/insightconnect-plugins/
 
 # updates - will ask for sudo password
-cd "$HOME/source_code/third_party/insightconnect-plugins/tools" || exit 5
-./update-tools.sh
+# this also has errors, may have to do with SentinelOne agent
+# npm ERR! Error: EPERM: operation not permitted, lchown '/usr/local/bin/sentinelctl'
+# cd "$HOME/source_code/third_party/insightconnect-plugins/tools" || exit 5
+# sudo ./update-tools.sh
 
-pip3 install --user insightconnect-integrations-validators pyyaml flake8
+# manual replacement for update-tools.sh:
+pip install --user insightconnect-integrations-validators pyyaml flake8
 npm install -g js-yaml
 brew install jq
 sudo gem install mdl
-
 
 ##############################################################################
 # validate dev env
@@ -72,17 +102,20 @@ sudo gem install mdl
 # start docker daemon first
 # do this in the GUI
 
-# Test: compile an existing plugin from another repo:
+# Test 1: compile an existing plugin from another repo:
 # change into the newly downloaded directory
-cd insightconnect-plugins/plugins/base64 || exit 2
+cd "$HOME/source_code/third_party/insightconnect-plugins/plugins/base64" || exit 2
 # build the Docker image for InsightConnect's base64 plugin - just to test
 # this takes several minutes
 icon-plugin build image
+# compile code, does not require sudo:
+make
 
 # Test #2 - building a new plugin
 # docker service must already be running
-cd "$HOME/source_code/presales-engineering-tim-dev/InsightConnect" || exit 7
-icon-plugin generate python example.yaml --path /tmp/example
+cd "$HOME/source_code/ubiquiti-udmp-api/insightconnect/plugins/example" || exit 7
+rm -Rf /tmp/example
+icon-plugin generate python plugin.spec.yaml --path /tmp/example
 cd /tmp/example/example || exit 8
 make
 # see the new image on localhost:

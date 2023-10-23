da4f015379d5a9b25f4bfe6a0692d1a7dc046b63
thonker-r7 <23639701+thonker-r7@users.noreply.github.com>
2023-10-23 14:20:44 -0400

    fixed comments, added line breaks

--- InsightVM/sql_reports/list-all-policies.sql
+++ InsightVM/sql_reports/list-all-policies.sql
@@ -1,7 +1,14 @@
-# Written 6/30/2020 by Tim H
-# lists all the built-in policies on a console
-# References: https://insightvm.help.rapid7.com/docs/understanding-the-reporting-data-model-dimensions
+-- Written 6/30/2020 by Tim H
+-- lists all the built-in policies on an InsightVM console
+-- this is only for network-based policy scans using authentication,
+-- this is not for agent based policy scans
+-- References: https://insightvm.help.rapid7.com/docs/understanding-the-reporting-data-model-dimensions
 
-select title, description, category, benchmark_name, benchmark_version from dim_policy
-where scope='Built-in'
+select 
+    title, description, category, benchmark_name, benchmark_version 
+
+from dim_policy
+
+where
+    scope='Built-in'
 order by title, benchmark_version

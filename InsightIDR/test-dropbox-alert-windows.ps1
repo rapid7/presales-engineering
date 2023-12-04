76e475f057cb4763e5ed5192e271511f188e4bb9
thonker-r7 <23639701+thonker-r7@users.noreply.github.com>
2023-12-04 15:06:40 -0500

    additional comments

--- InsightIDR/test-dropbox-alert-windows.ps1
+++ InsightIDR/test-dropbox-alert-windows.ps1
@@ -1,11 +1,16 @@
 # Dropbox test on Windows
+# Tim H 2023
 
 # view if Rapid7 agent is running - must be run in Powershell:
 Get-Service -Name ir_agent
+# The command above should return:
+# Status   Name               DisplayName
+# ------   ----               -----------
+# Running  ir_agent           Rapid7 Insight Agent
 
-# view the current timestamp on the Workstation/server:
+# view the current timestamp on the Workstation/server.
 # date
 
-# this DNS lookup should trigger an Alert in InsightIDR
-# visit the Alerts tab in InsightIDR
+# This DNS lookup should trigger an Alert in InsightIDR
+# visit the *Alerts* tab in InsightIDR
 nslookup content.dropboxapi.com
